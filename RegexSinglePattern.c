c25a061f6d2bc922740d3daebe363f8105e1facc
Amit Pathak <55128822+Viking-Programmer@users.noreply.github.com>
2019-09-16 00:28:18 +0530

    RegexSinglePattern.c
    This C program helps find a pattern in a string using regex.h library

---
+++ RegexSinglePattern.c
@@ -0,0 +1,23 @@
+//Created by Amit Pathak apathak092@gmail.com
+
+#include <regex.h>
+#include <stdio.h>
+
+int main() {
+
+regex_t preg;
+char *string = "ALIGN_QWERTY_KEY_UI; cd";
+char *pattern1 = "(^[A-Z])(([A-Z_]+_)?SK(_[A-Z_]+)?(;|$))";
+int rc;
+size_t nmatch = 2;
+regmatch_t pmatch[2];
+
+if ((rc = regcomp(&preg, pattern1, REG_EXTENDED)) == 0) {
+  if ((rc = regexec(&preg, string, nmatch, pmatch, 0)) != 0) {
+    printf("Match not present");
+  }else{
+    printf("Match present");
+}
+
+regfree(&preg);
+}
