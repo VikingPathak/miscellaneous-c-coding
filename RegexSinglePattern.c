fdee7ec29022b96c6c197ed39fcf80297c0b9712
Amit Pathak <55128822+Viking-Programmer@users.noreply.github.com>
2019-09-16 00:36:30 +0530

    Update RegexSinglePattern.c

--- RegexSinglePattern.c
+++ RegexSinglePattern.c
@@ -5,19 +5,19 @@
 
 int main() {
 
-regex_t preg;
-char *string = "ALIGN_QWERTY_KEY_UI; cd";
-char *pattern1 = "(^[A-Z])(([A-Z_]+_)?SK(_[A-Z_]+)?(;|$))";
-int rc;
-size_t nmatch = 2;
-regmatch_t pmatch[2];
+  regex_t preg;
+  char *string = "ALIGN_QWERTY_KEY_UI; cd";
+  char *pattern1 = "(^[A-Z])(([A-Z_]+_)?SK(_[A-Z_]+)?(;|$))";
+  int rc;
+  size_t nmatch = 2;
+  regmatch_t pmatch[2];
 
-if ((rc = regcomp(&preg, pattern1, REG_EXTENDED)) == 0) {
-  if ((rc = regexec(&preg, string, nmatch, pmatch, 0)) != 0) {
-    printf("Match not present");
-  }else{
-    printf("Match present");
-}
+  if ((rc = regcomp(&preg, pattern1, REG_EXTENDED)) == 0) {
+    if ((rc = regexec(&preg, string, nmatch, pmatch, 0)) != 0) {
+       printf("Match not present");
+    }else{
+       printf("Match present");
+  }
 
-regfree(&preg);
+  regfree(&preg);
 }
